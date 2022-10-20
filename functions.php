59975046d31947730dafcc5152602d00dd73e4b9
hedlundemma <112821036+hedlundemma@users.noreply.github.com>
2022-10-20 13:27:40 +0200

    first function

--- functions.php
+++ functions.php
@@ -1,3 +1,47 @@
 <?php
 
 declare(strict_types=1);
+
+/** get random tree image file name
+ * @return string tree file name
+ */
+function getRandomTree(): string
+{
+    $trees = array('ask.jpg', 'beech.jpg', 'birch.jpg', 'lind.jpg', 'maple.jpg', 'oak.jpg');
+
+    return $trees[array_rand($trees)];
+}
+function getChatbotResponse(string $key): string
+{
+
+    $responseMap = [
+        'hi' => [
+            'hello',
+            'whats poppin',
+            'hiiiii',
+            'it is what it is'
+        ],
+        'trees' => [
+            'I like trees',
+            'trees are the greatest thing'
+        ],
+        'favorite' => [
+            'My favorite tree is birch',
+            'Go and hug your favorite tree'
+        ],
+    ];
+
+
+    // getting the responses from responseMap using the key
+    $keysResponses = $responseMap[$key];
+
+    // pick a random key 
+    $randomKey = array_rand($keysResponses);
+
+    //
+    $randomResponse = $keysResponses[$randomKey];
+
+    return $randomResponse;
+}
+
+echo getChatbotResponse('hi');
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 require(__DIR__ . '/functions.php');
-require(__DIR__ . '/header.php');
 require(__DIR__ . '/values.php');
+
+require(__DIR__ . '/header.php');
+
+?>
+
+<img src="/images/<?php echo getRandomTree(); ?>" width=300px height=300px alt="" />
+
+<?php
+
+?>
